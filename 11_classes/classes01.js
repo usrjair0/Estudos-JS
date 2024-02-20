4c4eb7201dc6fbd8b60022e79d0418f8da440573
usrjair0 <juniorsilvasobrinho1999@gmail.com>
2024-02-20 17:50:59 -0300

    commit some changes

--- 11_classes/classes01.js
+++ 11_classes/classes01.js
@@ -1,20 +1,31 @@
 const jogador = function (nome, posicao, numGols) {
-    Nome = nome,
-    Posicao = posicao,
-    Gols = numGols
+  (Nome = nome), (Posicao = posicao), (Gols = numGols);
 
-    this.getNome = function(){
-        return Nome
-    } //aqui estou criando uma função chamada getNome
-    this.getPos = function() {
-        return Posicao
-    }
-    this.getGols = function() {
-        return Gols
-    }
-}
+  this.getNome = function () {
+    return Nome;
+  }; //aqui estou criando uma função chamada getNome
+  this.getPos = function () {
+    return Posicao;
+  };
+  this.getGols = function () {
+    return Gols;
+  };
+};
 
 const Neymar = new jogador("Neymar", "Atacante", 400);
 console.log(Neymar.getGols());
 const Renato = new jogador("Renato", "Meio campo", 80);
 //essa é uma forma antiga de fazer, existe uma forma nova que é bem eficiente, será vista logo adiante.
+
+const jj = [];
+const jairo = new Object();
+jairo.nome = "jairinho";
+jairo.idade = 19;
+const jairo2 = new Object();
+jairo2.nome = "jairinho2";
+jairo2.idade = 19;
+jj.push(jairo);
+jj.push(jairo2)
+console.log(jj)
+console.log(jj[0].nome)
+
